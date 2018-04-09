2fb61964d3d684c23420d654a6c0f95be52b15e7
deadania <deadania14@gmail.com>
2018-04-09 20:13:12 +0700

    #6 signup success, migration to DJANGO 2, perubahan di urls.py tiap app dan urls pusat

---
+++ user/forms.py
@@ -0,0 +1,39 @@
+import re
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.forms import Textarea, ValidationError, ModelForm, TextInput
+from django.forms.fields import Field
+from .models import UserProfile
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        required=True,
+        label='Username',
+        widget=TextInput(attrs={'style': 'text-transform:lowercase;'})
+    )
+    first_name = forms.CharField(
+        required=True,
+        label='Nama Depan',
+        widget=TextInput(attrs={'style': 'text-transform:uppercase;'})
+    )
+    last_name = forms.CharField(
+        required=False,
+        help_text='Optional.',
+        label='Nama Belakang',
+        widget=TextInput(attrs={'style': 'text-transform:uppercase;'})
+    )
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+    def clean_mail(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("email telah terdigunakan")
+        return email
+
+class SignUpAddition(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('gender', 'phone',)
--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from . import views
 
-app_name = 'staticpage'
+app_name = 'user'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'signup/', views.signup, name='signup'),
 
 ]
